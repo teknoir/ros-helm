8a55856746a5549b8934cd8a147f0b3f96a9ec15
Anders Åslund <anders.aslund@teknoir.ai>
2026-04-14 14:38:26 +0200

    add GitHub Actions workflow for multi-platform Docker image build and push; include navigation launch file for tb3 headless simulation

---
+++ docker/src/tb3_headless_launch/tb3_headless_launch/launch/tb3_house_headless_navigation.launch.py
@@ -0,0 +1,108 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import AppendEnvironmentVariable
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+ROS_DISTRO = os.environ.get('ROS_DISTRO')
+
+def generate_launch_description():
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='-2.0')
+    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'worlds',
+        'turtlebot3_house.world'
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': ['--headless-rendering -r -s -v2 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '--headless-rendering -g -v2 ', 'on_exit_shutdown': 'true'}.items()
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('turtlebot3_navigation2'),
+            'map',
+            'map.yaml'))
+
+    param_file_name = TURTLEBOT3_MODEL + '.yaml'
+    if ROS_DISTRO == 'humble':
+        param_dir = LaunchConfiguration(
+            'params_file',
+            default=os.path.join(
+                get_package_share_directory('turtlebot3_navigation2'),
+                'param',
+                ROS_DISTRO,
+                param_file_name))
+    else:
+        param_dir = LaunchConfiguration(
+            'params_file',
+            default=os.path.join(
+                get_package_share_directory('turtlebot3_navigation2'),
+                'param',
+                param_file_name))
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    nav2_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+        launch_arguments={
+            'map': map_dir,
+            'use_sim_time': use_sim_time,
+            'params_file': param_dir,
+            'autostart': 'True'}.items(),
+    )
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH',
+        os.path.join(
+            get_package_share_directory('turtlebot3_gazebo'),
+            'models'))
+
+    ld = LaunchDescription()
+
+    # Add the commands to the launch description
+    ld.add_action(gzserver_cmd)
+    # ld.add_action(gzclient_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(nav2_cmd)
+    ld.add_action(set_env_vars_resources)
+
+    return ld
